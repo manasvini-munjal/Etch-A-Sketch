41d6ac93c9232948b2a46fe125b70c8566584813
manasvinimunjal <manasvinimunjal@gmail.com>
2022-12-08 21:27:28 -0500

    Code grid layout

---
+++ script.js
@@ -0,0 +1,60 @@
+const GRID_SIZE = 16;
+const GRID_CONTAINER_SIZE = 960;
+const container = document.querySelector('.container');
+
+
+const createGrid = (gridSize) => {
+    const squareSize = `${GRID_CONTAINER_SIZE/gridSize}px`;
+    //console.log(`${GRID_CONTAINER_SIZE/gridSize}`);
+    //console.log(squareSize);
+    container.setAttribute('style', `grid-template-columns:repeat(${gridSize}, 0fr)`)
+    
+    for(let i = 1; i <= gridSize * gridSize; i++) {
+       
+      const grid = document.createElement('div');
+      grid.classList.add('grid');
+      grid.style.width = `${squareSize}`;
+      grid.style.height = `${squareSize}`;
+
+      grid.addEventListener('mouseover', () => {grid.style.background = 'red'});
+      grid.addEventListener('mouseout', () => {grid.style.background = 'transparent'});
+      container.appendChild(grid);
+    }
+}
+40
+const clearGrid = () => {
+    const grids = document.querySelectorAll('.grid');
+    
+    if (grids) {
+       // grid.parentNode.removeChild(grid);
+        //console.log('grid removed');
+    //console.log('grid exists');
+    grids.forEach(grid => {
+        grid.parentNode.removeChild(grid);
+    });
+       
+       // container.removeChild(container.firstChild);
+    }
+}
+
+const enterInput = () => {
+  const userInput = prompt("Enter number of squares for new grid", "");
+  const gridSize = parseInt(userInput);
+  console.log(userInput);
+  console.log(gridSize);
+  if(gridSize > 100) {
+    prompt("Enter a number less an 100");
+  } else if ( gridSize === GRID_SIZE) {
+    prompt(`${gridSize} * ${gridSize} grid already exists!`)
+  } else {
+     clearGrid();
+     createGrid(gridSize);
+  }
+}
+
+const gridSize = document.querySelector('.grid-size');
+gridSize.addEventListener('click', enterInput);
+window.addEventListener('load', () => {
+    createGrid(GRID_SIZE);
+});
+
